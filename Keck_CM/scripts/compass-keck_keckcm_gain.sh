fe82a61f07f499d9a361dc7ab5000c2b4cd4674e
asurendran-keck <59099115+asurendran-keck@users.noreply.github.com>
2020-01-15 09:29:41 -1000

    Update compass-keck_keckcm_gain.sh

--- Keck_CM/scripts/compass-keck_keckcm_gain.sh
+++ Keck_CM/scripts/compass-keck_keckcm_gain.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# This is the top level bash script which runs the compass-keck python script for the Keck CM pertaining to NGS mode
+# and can be used to benchmark the performance with parameters like zenith angle, magnitude and gain.
+# The example given here simulates the NGS mode with the Keck NGS parameter file and the Keck CM python script
+# It runs the python script for a sequence of different gains to measure the AO performance output
+
+# Setting the parameter file, python script file and result file names and paths
 script_name="compass-keck_keckcm_ngs_circ.py"
 param_name="scao_sh_20x20_4pix_keck_ngs_ml_tt.py"
 result_name="${param_name::-3}.txt"
-m='10'
-#r0=('0.16'
-#gain=($(seq 0.05 0.05 0.8))
-gain='0.2'
-#'0.1567' '0.1468' '0.13' '0.1056')
-#z=('0')
-niter=('10000')
-# '15' '30' '45' '60') # only used for altitude compensation
 path_script="/home/aodev/asurendran/scripts/test_scripts/main"
 path_params="/home/aodev/asurendran/param/1_recon"
 path_result="/home/aodev/asurendran/Results/1_recon"
+# Setting the gain array over which each python script will be run and number of AO loop iterations for every run
+m='10'
+gain=($(seq 0.05 0.05 0.8))
+niter=('10000')
+
+# Parsing filename for parameter, python script and result files
 script="$path_script/$script_name"
 param="$path_params/$param_name"
 output="$path_script/${param_name::-3}.log"
@@ -30,7 +34,9 @@ if [ -f "$output" ]; then
     echo "Log file exists, deleting the file"
     rm $output
 fi
+# Running the custom DM HDF5 script
 python h5panda.py
+# Iteration of the python script over all the gains
 for j in "${!gain[@]}"
   do
     CMD="python $script $param --save_filename "$savepath" --gain ${gain[$j]} --niter $niter"
